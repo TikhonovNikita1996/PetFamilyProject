0eaedf556904f2cf4bf533eaed33b0cdbe13fcd7
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2024-11-10 15:16:13 +0300

    Создана модель животного

---
+++ Backend/src/PetFamily.Domain/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.AccessControl;
+
+namespace PetFamily.Domain;
+
+public class Pet
+{
+    public Guid Id { get; set; }
+    public required string PetsName { get; set; }
+    public required string Species { get; set; }
+    public required string Description { get; set; }
+    public required string Breed { get; set; }
+    public required string Color { get; set; }
+    public required string HealthInformation { get; set; }
+    public required string LocationAdress { get; set; }
+    public required double Weight { get; set; }
+    public required double Hight { get; set; }
+    public required string OwnersPhoneNumber { get; set; }
+    public required bool IsSterilized { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+    public required bool IsVaccinated { get; set; }
+    public required string CurrentStatus { get; set; }
+    public required string DetailsforAssistance { get; set; }
+    public required DateTime PetsPageCreationDate { get; set; }
+}
