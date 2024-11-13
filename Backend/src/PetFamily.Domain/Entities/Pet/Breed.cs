b2d97e7c4988ea6200995365ab390d8a4313c069
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2024-11-13 16:37:16 +0300

    Промежуточные изменения

---
+++ Backend/src/PetFamily.Domain/Entities/Pet/Breed.cs
@@ -0,0 +1,13 @@
+﻿namespace PetFamily.Domain.Entities.Pet;
+
+public class Breed
+{
+    public Guid BreedId { get; private set; }
+    public string Name { get; private set; }
+
+    public Breed(Guid breedId, string name)
+    {
+        BreedId = breedId;
+        Name = name;
+    }
+}
---
+++ Backend/src/PetFamily.Infrastructure/Configurations/BreedConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.VisualBasic;
+using PetFamily.Domain.Entities.Pet;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+public class BreedConfiguration : IEntityTypeConfiguration<Breed>
+{
+    public void Configure(EntityTypeBuilder<Breed> builder)
+    {
+        builder.HasKey(b => b.BreedId);
+        
+        builder.Property(b => b.Name)
+            .IsRequired()
+            .HasMaxLength(ProjectConstants.MAX_LOW_TEXT_LENGTH);
+    }
+}
