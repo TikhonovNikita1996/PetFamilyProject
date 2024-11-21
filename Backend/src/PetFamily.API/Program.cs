f32b136cace777d5a6e65723dff8b2ba36bd5915
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2024-11-21 18:36:59 +0300

    added ExceptionMiddleware and seq logging

--- Backend/src/PetFamily.API/Program.cs
+++ Backend/src/PetFamily.API/Program.cs
@@ -1,17 +1,36 @@
+using PetFamily.API.Middlewares;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") ??
+                 throw new ArgumentNullException("Seq"))
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 builder.Services
     .AddInfrastructure()
     .AddApplication();
 
+builder.Services.AddSerilog();
+
 var app = builder.Build();
+
+app.UseExceptionMiddleware();
+app.UseSerilogRequestLogging();
 
 if (app.Environment.IsDevelopment())
 {
