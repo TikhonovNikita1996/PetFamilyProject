ece96418151c9e585baf8c0442ad7a6149db12cf
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-01-15 22:23:07 +0300

    Добавлены основные сущности

---
+++ Backend/src/Discussions/PetFamily.Discussions.Domain/Discussion.cs
@@ -0,0 +1,62 @@
+﻿using CSharpFunctionalExtensions;
+using Pet.Family.SharedKernel;
+
+namespace PetFamily.Discussions.Domain;
+
+public class Discussion
+{
+    public Guid Id { get; private set; }
+    public Guid RelationId { get; private set; }
+    public Relation Relation { get; private set; }
+    public List<Guid> UserIds { get; private set; } = [];
+    public List<Message> Messages { get; private set; } = [];
+    public DiscussionStatus Status { get; private set; }
+    
+    private Discussion() { }
+    
+    private Discussion(Guid relationId, IEnumerable<Guid> userIds)
+    {
+        Id = Guid.NewGuid();
+        RelationId = relationId;
+        UserIds = userIds.ToList();
+        Status = DiscussionStatus.Open;
+    }
+    
+    public void Close() => Status = DiscussionStatus.Closed;
+    public void Reopen() => Status = DiscussionStatus.Open;
+    
+    public static Result<Discussion, CustomError> Create(
+        Guid relationId, IEnumerable<Guid> userIds)
+    {
+        if (userIds.Count() != 2)
+            return Errors.General.Failure("There must be 2 users in discussion.");
+
+        return new Discussion(relationId, userIds);
+    }
+    
+    public UnitResult<CustomError> AddMessage(Message message)
+    {
+        if (Status == DiscussionStatus.Closed)
+            return Errors.General.Failure("Discussion is closed.");
+
+        bool isUserInCurrentDiscussion = UserIds.Contains(message.SenderId);
+
+        if (isUserInCurrentDiscussion is not true)
+            return Errors.General.Failure("User is not in this discussion.");
+
+        Messages.Add(message);
+        return Result.Success<CustomError>();
+    }
+    
+    public UnitResult<CustomError> RemoveMessage(Message message, Guid userId)
+    {
+        if (Status == DiscussionStatus.Closed)
+            return Errors.General.Failure("Discussion is closed.");
+
+        if (message.SenderId != userId)
+            return Errors.General.Failure("You can delete only your messages.");
+        
+        Messages.Remove(message);
+        return Result.Success<CustomError>();
+    }
+}
---
+++ Backend/src/Discussions/PetFamily.Discussions.Domain/Relation.cs
@@ -0,0 +1,18 @@
+﻿namespace PetFamily.Discussions.Domain;
+
+public class Relation
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    
+    public IReadOnlyList<Discussion> Discussions { get; private set; } = [];
+
+    private Relation() { }
+    private Relation(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+
+    public static Relation Create(string name) => new Relation(name);
+}
