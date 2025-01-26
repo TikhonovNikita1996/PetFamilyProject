47611efe20444b19d0612a72283e56b3abe07188
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-01-26 14:40:19 +0300

    Создан проект FileService

---
+++ Backend/src/Accounts/PetFamily.Accounts.Infrastructure/Migrations/20250119150616_Volunteer Account update.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Accounts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class VolunteerAccountupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "banned_for_requests_until",
+                schema: "PetFamily_Accounts",
+                table: "participant_accounts",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "banned_for_requests_until",
+                schema: "PetFamily_Accounts",
+                table: "participant_accounts");
+        }
+    }
+}
