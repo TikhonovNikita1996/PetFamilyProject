c3296ffa984c85f0021f982c264a1428480ccabd
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-02-05 17:38:37 +0300

    Доработан метод получения животного по Id с учетом взаимодействия с файловым сервисом.

--- Backend/src/Volunteers/PetFamily.Volunteers.Application/VolunteersManagement/Queries/GetPetById/GetPetByIdHandler.cs
+++ Backend/src/Volunteers/PetFamily.Volunteers.Application/VolunteersManagement/Queries/GetPetById/GetPetByIdHandler.cs
@@ -1,4 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
+using FileService.Communication;
+using FileService.Contracts;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Pet.Family.SharedKernel;
@@ -7,25 +9,29 @@ using PetFamily.Core.Dtos.Discussion;
 using PetFamily.Core.Dtos.Pet;
 using PetFamily.Core.Extensions;
 using PetFamily.Volunteers.Application.Database;
+using PetFamily.Volunteers.Contracts.Responses;
 
 namespace PetFamily.Volunteers.Application.VolunteersManagement.Queries.GetPetById;
 
-public class GetPetByIdHandler : IQueryHandler<Result<PetDto, CustomErrorsList>,
+public class GetPetByIdHandler : IQueryHandler<Result<PetResponse, CustomErrorsList>,
     GetPetByIdQuery>
 {
     private readonly IReadDbContext _readDbContext;
     private readonly IValidator<GetPetByIdQuery> _validator;
+    private readonly IFileService _fileService;
 
 
     public GetPetByIdHandler(
         IReadDbContext readDbContext, 
-        IValidator<GetPetByIdQuery> validator)
+        IValidator<GetPetByIdQuery> validator,
+        IFileService fileService)
     {
         _readDbContext = readDbContext;
         _validator = validator;
+        _fileService = fileService;
     }
 
-    public async Task<Result<PetDto, CustomErrorsList>> Handle(
+    public async Task<Result<PetResponse, CustomErrorsList>> Handle(
         GetPetByIdQuery query,
         CancellationToken cancellationToken)
     {
@@ -38,6 +44,20 @@ public class GetPetByIdHandler : IQueryHandler<Result<PetDto, CustomErrorsList>,
         var petDto = await speciesQuery.SingleOrDefaultAsync(v => v.Id == query.PetId
             ,cancellationToken);
         
-        return petDto;
+        var photoIds = petDto!.Photos.Select(d => d.FileId).ToList();
+        
+        var request = new GetFilePresignedUrlRequest(photoIds);
+        
+        var urlsResult = await _fileService.GetPresignedUrls(request, cancellationToken);
+
+        if (urlsResult.IsFailure)
+            return Errors.General.NotFound("urls").ToErrorList();
+
+        var photosIds = petDto.Photos.Select(p => p.FileId).ToList().AsReadOnly();
+        var photosUrls = urlsResult.Value.Select(r => r.PresignedUrl).ToList().AsReadOnly();
+
+        var petResponse = PetResponse.ToPetResponse(petDto, photosIds, photosUrls);
+        
+        return petResponse;
     }
 }
