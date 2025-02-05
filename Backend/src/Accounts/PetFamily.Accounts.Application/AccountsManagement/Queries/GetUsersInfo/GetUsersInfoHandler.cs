3d72fff0ef38fd00a94bce0700f4ef44e3991062
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-02-05 17:15:15 +0300

    Запрос пользователя из БД доработан под взаимодействие с файловым сервисом.

--- Backend/src/Accounts/PetFamily.Accounts.Application/AccountsManagement/Queries/GetUsersInfo/GetUsersInfoHandler.cs
+++ Backend/src/Accounts/PetFamily.Accounts.Application/AccountsManagement/Queries/GetUsersInfo/GetUsersInfoHandler.cs
@@ -1,30 +1,36 @@
 ﻿using CSharpFunctionalExtensions;
+using FileService.Communication;
+using FileService.Contracts;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Pet.Family.SharedKernel;
 using PetFamily.Accounts.Application.Database;
+using PetFamily.Accounts.Contracts.Responses;
 using PetFamily.Core.Abstractions;
 using PetFamily.Core.Dtos.Account;
 using PetFamily.Core.Extensions;
 
 namespace PetFamily.Accounts.Application.AccountsManagement.Queries.GetUsersInfo;
 
-public class GetUsersInfoHandler : IQueryHandler<Result<UserDto, CustomErrorsList>,
+public class GetUsersInfoHandler : IQueryHandler<Result<UserResponse, CustomErrorsList>,
     GetUsersInfoQuery>
 {
     private readonly IAccountsReadDbContext _readDbContext;
     private readonly IValidator<GetUsersInfoQuery> _validator;
+    private readonly IFileService _fileService;
 
 
     public GetUsersInfoHandler(
         IAccountsReadDbContext readDbContext, 
-        IValidator<GetUsersInfoQuery> validator)
+        IValidator<GetUsersInfoQuery> validator,
+        IFileService fileService)
     {
         _readDbContext = readDbContext;
         _validator = validator;
+        _fileService = fileService;
     }
 
-    public async Task<Result<UserDto, CustomErrorsList>> Handle(
+    public async Task<Result<UserResponse, CustomErrorsList>> Handle(
         GetUsersInfoQuery query,
         CancellationToken cancellationToken)
     {
@@ -38,6 +44,30 @@ public class GetUsersInfoHandler : IQueryHandler<Result<UserDto, CustomErrorsLis
             .Include(u => u.ParticipantAccount)
             .FirstOrDefaultAsync(u => u.Id == query.UserId, cancellationToken);
         
-        return userDto;
+        if (userDto is null)
+            return Errors.General.NotFound("user").ToErrorList();
+        
+        var request = new GetFilePresignedUrlRequest([userDto.Photo!.FileId]);
+        var urlResult = await _fileService.GetPresignedUrls(request, cancellationToken);
+        
+        if (urlResult.IsFailure)
+            return Errors.General.NotFound("url").ToErrorList();
+
+        var userResponse = new UserResponse
+        {
+            Id = userDto.Id,
+            LastName = userDto.LastName,
+            Name = userDto.Name,
+            MiddleName = userDto.MiddleName,
+            UserName = userDto.UserName,
+            Email = userDto.Email,
+            PhotoId = userDto.Photo.FileId,
+            PhotoUrl = urlResult.Value.FirstOrDefault()!.PresignedUrl,
+            AdminAccount = userDto.AdminAccount,
+            VolunteerAccount = userDto.VolunteerAccount,
+            ParticipantAccount = userDto.ParticipantAccount
+        };
+        
+        return userResponse;
     }
 }
---
+++ Backend/src/Accounts/PetFamily.Accounts.Contracts/Responses/UserResponse.cs
@@ -0,0 +1,18 @@
+﻿using PetFamily.Core.Dtos.Account;
+
+namespace PetFamily.Accounts.Contracts.Responses;
+
+public class UserResponse
+{
+    public Guid Id { get; init; }
+    public string LastName { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public string? MiddleName { get; init; } = string.Empty;
+    public string UserName { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public Guid PhotoId { get; init; }
+    public string PhotoUrl { get; init; } = string.Empty;
+    public AdminAccountDto? AdminAccount { get; init; }
+    public VolunteerAccountDto? VolunteerAccount { get; init; }
+    public ParticipantAccountDto? ParticipantAccount { get; init; } = null!;
+}
