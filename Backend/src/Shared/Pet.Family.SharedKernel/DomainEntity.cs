43381e7a4cebde2a643bc63876bb9d810f83e979
TikhonovNikita1996 <nikita.tikonov1996@bk.ru>
2025-02-19 11:03:45 +0300

    Rabbit запущен в докере

--- Backend/src/Shared/Pet.Family.SharedKernel/DomainEntity.cs
+++ Backend/src/Shared/Pet.Family.SharedKernel/DomainEntity.cs
@@ -15,7 +15,5 @@ public abstract class DomainEntity<TId> : Entity<TId>
 
     protected void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
     
-    protected void RemoveDomainEvent(IDomainEvent domainEvent) => _domainEvents.Remove(domainEvent);
-    
     public void ClearDomainEvents() => _domainEvents.Clear();
 }
